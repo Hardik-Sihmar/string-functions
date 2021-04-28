88e692c6f8107e4b15b3d8bca5820d8599d9ed0b
Hardik-Sihmar <83064171+Hardik-Sihmar@users.noreply.github.com>
2021-04-28 16:58:23 +0530

    Add files via upload

---
+++ stringfunctions.py
@@ -0,0 +1,31 @@
+
+# Some functions of Strings
+
+hello = "my name is Hardik"
+
+standard = "I am in eleventh standard."
+
+print(hello + standard)
+
+# 1. Len
+print(len(hello))
+
+# 2. endswith
+print(hello.endswith('Hardik'))
+
+# 3. capitalize
+print(hello.capitalize())
+
+# 4. count
+print(hello.count('a'))
+
+# 5. replace
+print(hello.replace('name', 'hardik'))
+
+# 6. find
+print(hello.find('hardik'))
+
+
+
+
+
